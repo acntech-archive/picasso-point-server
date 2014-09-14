f92055b2c2937d6d02c6c4a73e8e536895f7bd2c
Simen Bekkhus <sbekkhus91@gmail.com>
2014-09-14 17:37:08 +0200

    Use only a single socket

--- src/server.js
+++ src/server.js
@@ -5,36 +5,34 @@ var WebSocketServer = ws.Server;
 var http = require('http');
 var express = require('express');
 var _ = require('lodash');
-var jrc = require('just.randomcolor');
 var app = express();
 var inputPort = process.env.PORT || 5000;
 
 var uniqueUsers = [];
+var uniqueColors = ['#abbf72', '#72bf87', '#72bfa2', '#8ebf72', '#72bf96', '#72bfa3', '#bf72a4', '#75bf72', '#bf8672', '#bfbf72', '#72bfa5', '#9f72bf', '#72bfaf', '#7872bf', '#bf727e', '#72a0bf', '#bf7279', '#a572bf', '#72bf7b', '#7299bf'];
 
 app.use(express.static(__dirname + '/'));
 
-var inputServer = http.createServer(app);
-var outputServer = http.createServer(app);
-inputServer.listen(inputPort);
-
-var wsServer = new WebSocketServer({
-  server: inputServer
+app.get('/port-number', function (req, res) {
+  res.send(JSON.stringify({ port: inputPort }));
 });
 
-var clients = [];
+var server = http.createServer(app);
+server.listen(inputPort);
 
-function doesUserExist(user) {
-  return _.pluck(uniqueUsers, {
-    user: user
-  });
-}
+var wsServer = new WebSocketServer({
+  server: server
+});
+
+var clients = [], canvases = [];
 
 wsServer.on('connection', function (ws) {
   console.log('websocket input got connection');
 
   if (ws.protocol === 'client') {
     clients.push(ws);
-    console.log('websocket get a client');
+  } else if (ws.protocol === 'canvas') {
+    canvases.push(ws);
   }
 
   ws.on('close', function () {
@@ -42,29 +40,28 @@ wsServer.on('connection', function (ws) {
   });
 
   ws.on('message', function (msg) {
-    console.log(msg);
-    var user = JSON.parse(msg).user;
+      var parsedMsg = JSON.parse(msg);
 
-    /* jshint camelcase: false */
-    _(clients)
-      .filter(function (e) {
-        return e.protocol === 'client';
-      })
-      .each(function (e) {
-        try {
-          if (user) {
-            if (!doesUserExist(user)) {
-              uniqueUsers.push({
-                user: user,
-                color: new jrc()
-              });
-            }
+      if (parsedMsg.user) {
+        var newUser = {
+          user: parsedMsg.user + '#' + uniqueUsers.length,
+          color: uniqueColors[uniqueUsers.length]
+        };
+        console.log('Created user: ' + newUser.user);
+        ws.send(JSON.stringify(newUser));
+      } else if (parsedMsg.userId) {
+        canvases.forEach(function (e) {
+          try {
+            e.send(msg);
+            console.log('sending message');
+          } catch (e) {
+            console.log('ohnnoes:' + e);
           }
-          e.send(msg);
-          console.log('sending message');
-        } catch (e) {
-          console.log('ohnnoes:' + e);
-        }
-      });
-  });
+        });
+      }
+      else {
+        console.log('sa;dofjbugfdsljhbdwpfiuhbn')
+      }
+    }
+  );
 });
